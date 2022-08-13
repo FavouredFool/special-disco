5100327dde558b93abe85131277ca4a736b76bb1
Tim Wahrburg <t.wahrburg@gmail.com>
2022-08-13 22:43:19 +0200

    feat: added dashed line

--- Scripts/BallSpawner.gd
+++ Scripts/BallSpawner.gd
@@ -13,8 +13,15 @@ export var hold_increase : float = 300
 
 onready var throw_strength = min_throw_strength
 
+func _draw():
+	print(get_viewport().get_mouse_position())
+	var draw_goal = player.position + (get_viewport().get_mouse_position() - player.position).normalized() * throw_strength / 8
+	
+	draw_set_transform_matrix(transform.inverse())
+	draw_dashed_line(player.position, draw_goal, Color(1, 1, 1, 1), 5, 10, false)
 
 func _process(delta):
+	update()
 	if not player.get_node("RingSelection").visible:
 		if Input.is_mouse_button_pressed(1):
 			if ball_instance:
@@ -51,3 +58,28 @@ func instantiate_ball_for_dog(var position : Vector2):
 	add_child(ball_instance)
 	ball_instance.add_to_group("pickupable")
 	ball_instance.position = position
+	
+func draw_dashed_line(from, to, color, width, dash_length = 5, cap_end = false, antialiased = false):
+	#https://github.com/juddrgledhill/godot-dashed-line
+	var length = (to - from).length()
+	var normal = (to - from).normalized()
+	var dash_step = normal * dash_length
+	
+	if length < dash_length: #not long enough to dash
+		draw_line(from, to, color, width, antialiased)
+		return
+
+	else:
+		var draw_flag = true
+		var segment_start = from
+		var steps = length/dash_length
+		for start_length in range(0, steps + 1):
+			var segment_end = segment_start + dash_step
+			if draw_flag:
+				draw_line(segment_start, segment_end, color, width, antialiased)
+
+			segment_start = segment_end
+			draw_flag = !draw_flag
+		
+		if cap_end:
+			draw_line(segment_start, to, color, width, antialiased)
