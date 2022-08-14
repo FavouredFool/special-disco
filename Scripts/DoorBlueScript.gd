28b43c266401f9caa557a5751cb479e11db411d9
Tim Wahrburg <t.wahrburg@gmail.com>
2022-08-14 13:06:54 +0200

    feat: Added Doorlogic

--- Scripts/DoorBlueScript.gd
+++ Scripts/DoorBlueScript.gd
@@ -1,20 +1,28 @@
 extends Node2D
 
 export var perm_open = false
-export var color : Color
+
+var permTexture = preload("res://Doors/LaserbeamBlueB.png")
+var permTextureEmpty = preload("res://Doors/LaserbeamEmptyB.png")
+var textureEmpty = preload("res://Doors/LaserbeamEmpty.png")
+var texture = preload("res://Doors/LaserbeamBlue.png")
 
 func _ready():
-	if not perm_open:
-		$ColorRect2.visible = true
+	if perm_open:
+		$Sprite.texture = permTexture
+	else:
+		$Sprite.texture = texture
 
 func _on_ButtonBlue_button_blue_pressed():
 	$StaticBody2D/CollisionShape2D.disabled = true
-	$ColorRect.visible = false
-	$ColorRect2.visible = false
+	if perm_open:
+		$Sprite.texture = permTextureEmpty
+	else:
+		$Sprite.texture = textureEmpty
+		
 
 
 func _on_ButtonBlue_button_blue_released():
 	if not perm_open:
 		$StaticBody2D/CollisionShape2D.disabled = false
-		$ColorRect.visible = true
-		$ColorRect2.visible = true
+		$Sprite.texture = texture
