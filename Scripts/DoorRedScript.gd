7fe738bd79b188caae7771d6e2a30a0a8b1145f7
Tim Wahrburg <t.wahrburg@gmail.com>
2022-08-15 13:53:27 +0200

    feat: doors can be reversed

--- Scripts/DoorRedScript.gd
+++ Scripts/DoorRedScript.gd
@@ -1,7 +1,9 @@
 extends Node2D
 
-export var perm_open = false
-var is_open = false
+export var perm_activated = false
+export var reversed = false
+var is_open_perm= false
+var is_closed_perm= false
 var first_frame_released = false
 var first_frame_pressed = false
 var permTexture = preload("res://Doors/LaserbeamRedB.png")
@@ -10,32 +12,70 @@ var textureEmpty = preload("res://Doors/LaserbeamEmpty.png")
 var texture = preload("res://Doors/LaserbeamRed.png")
 
 func _ready():
-	if perm_open:
+	if perm_activated:
 		$Sprite.texture = permTexture
 	else:
 		$Sprite.texture = texture
 
 func _on_ButtonRed_button_red_pressed():
-	$StaticBody2D/CollisionShape2D.disabled = true
-	if perm_open:
-		if not is_open and first_frame_pressed:
-			$Laser_down.play()
-		$Sprite.texture = permTextureEmpty
-		is_open = true
+	if reversed:
+		if not is_open_perm:
+			door_up()
 	else:
-		$Sprite.texture = textureEmpty
-		if first_frame_pressed:
-			$Laser_down.play()
-	first_frame_released = true
-	first_frame_pressed = false
+		if not is_closed_perm:
+			door_down()
 		
 
 
 func _on_ButtonRed_button_red_released():
-	if not perm_open:
-		$StaticBody2D/CollisionShape2D.disabled = false
+	if reversed:
+		if not is_closed_perm:
+			door_down()
+	else:
+		if not is_open_perm:
+			door_up()
+
+func door_down():
+	$StaticBody2D/CollisionShape2D.disabled = true
+	
+	if perm_activated:
+		if not is_open_perm:
+			if first_frame_pressed:
+					$Laser_down.play()
+			
+			$Sprite.texture = permTextureEmpty
+			if not reversed:
+				is_open_perm = true
+	else:
+		$Sprite.texture = textureEmpty
+		if reversed:
+			if first_frame_pressed:
+				$Laser_down.play()
+		else:
+			if first_frame_pressed:
+				$Laser_down.play()
+		
+
+	first_frame_released = true
+	first_frame_pressed = false
+
+
+	
+func door_up():
+	$StaticBody2D/CollisionShape2D.disabled = false
+	
+	if perm_activated:
+		if not is_closed_perm:
+			if first_frame_released:
+				$Laser_up.play()
+
+			$Sprite.texture = permTexture
+			if reversed:
+				is_closed_perm = true
+	else:
 		$Sprite.texture = texture
 		if first_frame_released:
 			$Laser_up.play()
+
 	first_frame_released = false
 	first_frame_pressed = true
